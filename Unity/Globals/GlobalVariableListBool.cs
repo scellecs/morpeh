ba593b5305551cd2c7ddf32cf8c4ef2266ad3d19
GolovchanskyVA <golovchanskyva@plavalaguna.ru>
2020-02-21 15:26:42 +0300

    fix global variables list

--- Unity/Globals/GlobalVariableListBool.cs
+++ Unity/Globals/GlobalVariableListBool.cs
@@ -1,6 +1,7 @@
 namespace Morpeh.Globals {
     using System.Collections.Generic;
     using UnityEngine;
+    using System;
 #if ODIN_INSPECTOR
     using Sirenix.OdinInspector;
     
@@ -9,8 +10,19 @@ namespace Morpeh.Globals {
     [CreateAssetMenu(menuName = "ECS/Globals/Variable List Bool")]
     public class GlobalVariableListBool : BaseGlobalVariable<List<bool>> {
         protected override List<bool> Load(string serializedData)
-            => JsonUtility.FromJson<List<bool>>(serializedData);
+            => JsonUtility.FromJson<ListBoolWrapper>(serializedData).list;
 
-        protected override string Save() => JsonUtility.ToJson(this.value);
+        protected override string Save() => JsonUtility.ToJson(new ListBoolWrapper(this.value));
+    }
+
+    [Serializable]
+    struct ListBoolWrapper
+    {
+        public List<bool> list;
+
+        public ListBoolWrapper(List<bool> list)
+        {
+            this.list = list;
+        }
     }
 }
--- Unity/Globals/GlobalVariableListInt.cs
+++ Unity/Globals/GlobalVariableListInt.cs
@@ -1,6 +1,7 @@
 namespace Morpeh.Globals {
     using System.Collections.Generic;
     using UnityEngine;
+    using System;
 #if ODIN_INSPECTOR
     using Sirenix.OdinInspector;
     
@@ -9,8 +10,19 @@ namespace Morpeh.Globals {
     [CreateAssetMenu(menuName = "ECS/Globals/Variable List Int")]
     public class GlobalVariableListInt : BaseGlobalVariable<List<int>> {
         protected override List<int> Load(string serializedData)
-            => JsonUtility.FromJson<List<int>>(serializedData);
+            => JsonUtility.FromJson<ListIntWrapper>(serializedData).list;
 
-        protected override string Save() => JsonUtility.ToJson(this.value);
+        protected override string Save() => JsonUtility.ToJson(new ListIntWrapper(this.value));
+    }
+
+    [Serializable]
+    struct ListIntWrapper
+    {
+        public List<int> list;
+
+        public ListIntWrapper(List<int> list)
+        {
+            this.list = list;
+        }
     }
 }
