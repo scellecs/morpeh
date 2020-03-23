aeffd8593a67803c46785d5ff482c5cbbe329c0f
k0fe <x.k0fe@yandex.ru>
2020-03-23 18:55:12 +0300

    moved and restyled

--- Unity/Providers/BaseComponentProvider.cs
+++ Unity/Providers/BaseComponentProvider.cs
@@ -1,16 +1,12 @@
-﻿namespace Morpeh
-{
+﻿namespace Morpeh {
     using UnityEngine.Assertions;
-
+ 
     public abstract class BaseComponentProvider<T0, T1> : MonoProvider<T1>
         where T0 : UnityEngine.Component
-        where T1 : struct, IMonoComponent<T0>
-    {
-        private void OnValidate()
-        {
+        where T1 : struct, IMonoComponent<T0> {
+            private void OnValidate() {
             ref var data = ref GetData(out _);
-            if (data.monoComponent == null)
-            {
+            if (data.monoComponent == null) {
                 data.monoComponent = this.gameObject.GetComponent<T0>();
                 Assert.IsNotNull(data.monoComponent, $"Missing {typeof(T0)} component.");
             }
