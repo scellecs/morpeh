78ecd674da42b1a91024a30d567a8f816d34dd41
Oleg Morozov <benjminmoore@gmail.com>
2020-04-02 17:06:41 +0300

    Discover Actions Odin Button

--- Unity/Utils/Editor/Discover/DiscoverAction.cs
+++ Unity/Utils/Editor/Discover/DiscoverAction.cs
@@ -4,6 +4,9 @@
     public abstract class DiscoverAction : ScriptableObject {
         public abstract string ActionName { get; }
 
+#if ODIN_INSPECTOR
+        [Sirenix.OdinInspector.Button(Name = "@ActionName")]
+#endif
         public abstract void DoAction();
     }
 }
