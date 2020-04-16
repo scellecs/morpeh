cc6572797944c7ab9fb193684f2cb1e6820cbd9d
golovchanskyva <golovchanskyva@plavalaguna.ru>
2020-04-17 00:50:36 +0300

    bigNumber on bigInteger

---
+++ Unity/Globals/Variables/GlobalVariableBigInteger.cs
@@ -0,0 +1,44 @@
+﻿using System.Numerics;
+using Morpeh.BigNumber;
+using Sirenix.OdinInspector;
+
+namespace Morpeh.Globals {
+    using System.Globalization;
+    using UnityEngine;
+    using Unity.IL2CPP.CompilerServices;
+
+    [Il2CppSetOption(Option.NullChecks, false)]
+    [Il2CppSetOption(Option.ArrayBoundsChecks, false)]
+    [Il2CppSetOption(Option.DivideByZeroChecks, false)]
+    [CreateAssetMenu(menuName = "ECS/Globals/Variable Big Integer")]
+    public class GlobalVariableBigInteger : BaseGlobalVariable<BigInt> {
+        public override IDataWrapper Wrapper {
+            get => new BigIntegerWrapper {value = this.value};
+            set => this.Value = ((BigIntegerWrapper) value).value;
+        }
+        
+        protected override BigInt Load(string serializedData) {
+            return BigInt.Parse(serializedData);
+        }
+
+        protected override string Save() {
+            var str = this.value.ToBigIntegerString();
+            return str;
+        }
+
+        [System.Serializable]
+        private class BigIntegerWrapper : IDataWrapper {
+            public BigInt value;
+        }
+        
+#if UNITY_EDITOR && ODIN_INSPECTOR
+        [InlineButton("UpdateValue")]
+        public string newValue;
+
+        private void UpdateValue() {
+            this.value.value = BigInteger.Parse(newValue);
+            this.SaveData();
+        }     
+#endif
+    }
+}
---
+++ Unity/Utils/BigNumber/BigInt.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Numerics;
+using Sirenix.OdinInspector;
+using UnityEngine;
+
+namespace Morpeh.BigNumber {
+    [Serializable]
+    public struct BigInt {
+        
+#if UNITY_EDITOR && ODIN_INSPECTOR
+       // [ReadOnly]
+        //public string stringValue;
+#endif
+        
+        [HideLabel]
+        public BigInteger value;
+
+        public BigInt(int value) {
+            this.value = value;
+#if UNITY_EDITOR && ODIN_INSPECTOR
+            //this.stringValue = value.ToString();
+#endif
+        }
+        
+        public BigInt(BigInteger value) {
+            this.value = value;
+#if UNITY_EDITOR && ODIN_INSPECTOR
+            //this.stringValue = value.ToString();
+#endif
+        }
+
+        public override string ToString() => this.value.ToString();
+
+        public string ToBigIntegerString() => this.value.ToString();
+
+        public static BigInt Parse(string value) {
+            return new BigInt(BigInteger.Parse(value));
+        }
+
+        public static BigInt operator +(BigInt left, int right) => new BigInt(left.value + right);
+        public static BigInt operator +(BigInt left, BigInt right) => new BigInt(left.value + right.value);
+    }
+}
