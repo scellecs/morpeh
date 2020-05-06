06947f5e30126b01117ec5e37ddca9efa4a418c4
Oleg Morozov <benjminmoore@gmail.com>
2020-05-06 12:35:44 +0300

    Add overloads for GlobalEvents

--- Unity/Globals/Events/GlobalEventBool.cs
+++ Unity/Globals/Events/GlobalEventBool.cs
@@ -1,4 +1,5 @@
 ﻿namespace Morpeh.Globals {
+    using System.Globalization;
     using Unity.IL2CPP.CompilerServices;
     using UnityEngine;
 
@@ -7,5 +8,6 @@
     [Il2CppSetOption(Option.DivideByZeroChecks, false)]
     [CreateAssetMenu(menuName = "ECS/Globals/Event Bool")]
     public class GlobalEventBool : BaseGlobalEvent<bool> {
+        public override string LastToString() => this.BatchedChanges.Peek().ToString(CultureInfo.InvariantCulture);
     }
 }
--- Unity/Globals/Events/GlobalEventFloat.cs
+++ Unity/Globals/Events/GlobalEventFloat.cs
@@ -1,4 +1,5 @@
 namespace Morpeh.Globals {
+    using System.Globalization;
     using UnityEngine;
     using Unity.IL2CPP.CompilerServices;
 
@@ -7,5 +8,6 @@ namespace Morpeh.Globals {
     [Il2CppSetOption(Option.DivideByZeroChecks, false)]
     [CreateAssetMenu(menuName = "ECS/Globals/Event Float")]
     public class GlobalEventFloat : BaseGlobalEvent<float> {
+        public override string LastToString() => this.BatchedChanges.Peek().ToString(CultureInfo.InvariantCulture);
     }
 }
