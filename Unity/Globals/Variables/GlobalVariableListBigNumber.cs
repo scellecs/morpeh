96574f37edfb21f6c52f5bfd663d65da7d92e733
Oleg Morozov <benjminmoore@gmail.com>
2020-05-07 18:17:01 +0300

    Fix merge

--- Unity/Globals/Variables/GlobalVariableListBigNumber.cs
+++ Unity/Globals/Variables/GlobalVariableListBigNumber.cs
@@ -9,34 +9,32 @@
     [Il2CppSetOption(Option.ArrayBoundsChecks, false)]
     [Il2CppSetOption(Option.DivideByZeroChecks, false)]
     [CreateAssetMenu(menuName = "ECS/Globals/Variable List Big Number")]
-    public class GlobalVariableListBigNumber : BaseGlobalVariable<List<BigNumber>>
-    {
+    public class GlobalVariableListBigNumber : BaseGlobalVariable<List<BigNumber>> {
         public override DataWrapper Wrapper {
             get => new ListBigNumberWrapper(this.value);
             set => this.Value = ((ListBigNumberWrapper) value).ToBigNumberList();
         }
-        
-        protected override List<BigNumber> Load(string serializedData) => 
+
+        protected override List<BigNumber> Load(string serializedData) =>
             JsonUtility.FromJson<ListBigNumberWrapper>(serializedData).ToBigNumberList();
 
         protected override string Save() {
-            return JsonUtility.ToJson(new ListBigNumberWrapper (this.value));
+            return JsonUtility.ToJson(new ListBigNumberWrapper(this.value));
         }
-        
+
         internal override void OnEnable() {
-            if (this.defaultValue != null)
-            {
+            if (this.defaultValue != null) {
                 this.value = ListBigNumberWrapper.ConvertToBigNumberList(this.defaultValue);
             }
 
             base.OnEnable();
         }
-        
+
         public override string LastToString() => string.Join(",", this.BatchedChanges.Peek());
         [SerializeField]
         private List<string> defaultValue;
-        public List<string> DefaultValue
-        {
+
+        public List<string> DefaultValue {
             private get => this.defaultValue;
             set => this.defaultValue = value;
         }
@@ -46,17 +44,15 @@
         [Il2CppSetOption(Option.DivideByZeroChecks, false)]
         [System.Serializable]
         private class ListBigNumberWrapper : DataWrapper {
-			public List<string> list;
-            
-			public override string ToStri            public override string ToString() => string.Join(",", this.list);
             public List<string> list;
+
+            public override string ToString() => string.Join(",", this.list);
 
             public ListBigNumberWrapper(List<BigNumber> bigList) {
                 this.list = new List<string>();
-                if(bigList == null) return;
-                
-                foreach (var big in bigList)
-                {
+                if (bigList == null) return;
+
+                foreach (var big in bigList) {
                     this.list.Add(big.ToBigIntegerString());
                 }
             }
@@ -67,8 +63,7 @@
 
             public static List<BigNumber> ConvertToBigNumberList(List<string> list) {
                 var result = new List<BigNumber>();
-                foreach (var str in list)
-                {
+                foreach (var str in list) {
                     result.Add(BigNumber.Parse(str));
                 }
 
