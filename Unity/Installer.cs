d3a62258dc19e5bd1e4e0e78561d3a430834b4eb
Alexandr Semenov <warmerko@gmail.com>
2020-05-12 15:16:44 +0400

    Pre calculate orders in edit mode

--- Unity/Installer.cs
+++ Unity/Installer.cs
@@ -2,6 +2,9 @@
 #if UNITY_EDITOR
     using UnityEditor;
 #endif
+    using System;
+    using System.Collections;
+    using System.Collections.Generic;
     using System.Linq;
     using UnityEngine;
     using Utils;
@@ -9,14 +12,20 @@
     using Sirenix.OdinInspector;
 #endif
     using Unity.IL2CPP.CompilerServices;
-
+    using UnityEngine.SceneManagement;
+    
+    [ExecuteInEditMode]
     [Il2CppSetOption(Option.NullChecks, false)]
     [Il2CppSetOption(Option.ArrayBoundsChecks, false)]
     [Il2CppSetOption(Option.DivideByZeroChecks, false)]
     [AddComponentMenu("ECS/" + nameof(Installer))]
     public sealed class Installer : BaseInstaller {
-        private int order;
-        
+#if UNITY_EDITOR && ODIN_INSPECTOR
+        [Required]
+        [PropertyOrder(-5)]
+#endif
+        public int order;
+
         [Space]
 #if UNITY_EDITOR && ODIN_INSPECTOR
         [PropertyOrder(-5)]
@@ -61,9 +70,92 @@
             }
         }
 
+#if UNITY_EDITOR
+        private const int MAX_INSTALLERS_IN_SCENE = 1000;
+        private int lastSiblingIndex = -1;
+
+        private void Update() {
+            if (Application.isPlaying) {
+                return;
+            }
+
+            var currentSiblingIndex = this.transform.GetSiblingIndex();
+            
+            if (currentSiblingIndex == this.lastSiblingIndex) {
+                return;
+            }
+
+            this.lastSiblingIndex = currentSiblingIndex;
+
+            this.CalculateOrders();
+        }
+
+        private void OnTransformParentChanged() {
+            this.CalculateOrders();
+        }
+
+        private void CalculateOrders() {
+            var installers = new List<Installer>(FindObjectsOfType<Installer>());
+            var scene      = this.gameObject.scene;
+            
+            if (scene.name == null) {
+                return;
+            }
+            
+            var sceneOrderOffset = scene.buildIndex * MAX_INSTALLERS_IN_SCENE;
+
+            List<Transform> GetParents(Transform t) {
+                var parents = new List<Transform> {t};
+
+                while (t.parent != null) {
+                    var parent = t.parent;
+                    parents.Add(parent);
+                    t = parent;
+                }
+
+                return parents;
+            }
+
+            installers.Sort((x, y) => {
+                if (x == y)
+                    return 0;
+
+                if (y.transform.IsChildOf(x.transform)) {
+                    return -1;
+                }
+
+                if (x.transform.IsChildOf(y.transform)) {
+                    return 1;
+                }
+
+                var xParentList = GetParents(x.transform);
+                var yParentList = GetParents(y.transform);
+
+                for (var xIndex = 0; xIndex < xParentList.Count; xIndex++) {
+                    if (!y.transform.IsChildOf(xParentList[xIndex])) {
+                        continue;
+                    }
+
+                    var yIndex = yParentList.IndexOf(xParentList[xIndex]) - 1;
+                    xIndex -= 1;
+                    return xParentList[xIndex].GetSiblingIndex() - yParentList[yIndex].GetSiblingIndex();
+                }
+
+                return xParentList[xParentList.Count - 1].GetSiblingIndex() - yParentList[yParentList.Count - 1].GetSiblingIndex();
+            });
+
+            for (var i = 0; i < installers.Count; ++i) {
+                installers[i].order = sceneOrderOffset + i;
+            }
+        }
+#endif
+
         protected override void OnEnable() {
+            if (Application.isPlaying == false) {
+                return;
+            }
+            
             this.group = World.Default.CreateSystemsGroup();
-            this.order = this.transform.GetSiblingIndex();
             
             for (int i = 0, length = this.initializers.Length; i < length; i++) {
                 var initializer = this.initializers[i];
@@ -78,6 +170,10 @@
         }
 
         protected override void OnDisable() {
+            if (Application.isPlaying == false) {
+                return;
+            }
+            
             this.RemoveSystems(this.updateSystems);
             this.RemoveSystems(this.fixedUpdateSystems);
             this.RemoveSystems(this.lateUpdateSystems);
