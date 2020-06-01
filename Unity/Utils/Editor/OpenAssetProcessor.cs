722e1f388e6b1f65f1d9e65bbfb4fecbaa412ef5
Oleg Morozov <benjminmoore@gmail.com>
2020-06-01 13:52:09 +0300

    Open System Asset Script

---
+++ Unity/Utils/Editor/OpenAssetProcessor.cs
@@ -0,0 +1,22 @@
+﻿#if UNITY_EDITOR
+namespace Morpeh.Utils.Editor {
+    using UnityEditor;
+    using UnityEditor.Callbacks;
+    using UnityEngine;
+
+    public static class OpenAssetProcessor {
+        [OnOpenAsset(-1000)]
+        public static bool OnOpenAsset(int instanceID, int line) {
+            var obj = EditorUtility.InstanceIDToObject(instanceID);
+            if (obj is ScriptableObject so) {
+                if (so is IInitializer && so is Configurator == false) {
+                    var monoScript = MonoScript.FromScriptableObject(so);
+                    AssetDatabase.OpenAsset(monoScript);
+                    return true;
+                }
+            }
+            return false;
+        }
+    }
+}
+#endif
