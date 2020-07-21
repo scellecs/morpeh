10327855dd188ccd8eb2e0712fc91234b8e98c3b
Oleg Morozov <benjminmoore@gmail.com>
2020-07-21 17:26:31 +0300

    FastListExtensions

--- Morpeh.cs
+++ Morpeh.cs
@@ -3226,6 +3226,7 @@ namespace Morpeh {
                 }
             }
 
+            [MethodImpl(MethodImplOptions.AggressiveInlining)]
             public static void Clear(this IntStack stack) {
                 stack.data   = null;
                 stack.length = stack.capacity = 0;
@@ -3269,111 +3270,6 @@ namespace Morpeh {
                 Array.Copy(other.data, 0, this.data, 0, this.length);
 
                 this.comparer = other.comparer;
-            }
-
-            [MethodImpl(MethodImplOptions.AggressiveInlining)]
-            public int Add() {
-                var index = this.length;
-                if (++this.length == this.capacity) {
-                    ArrayHelpers.Grow(ref this.data, this.capacity <<= 1);
-                }
-
-                return index;
-            }
-
-            [MethodImpl(MethodImplOptions.AggressiveInlining)]
-            public int Add(T value) {
-                var index = this.length;
-                if (++this.length == this.capacity) {
-                    ArrayHelpers.Grow(ref this.data, this.capacity <<= 1);
-                }
-
-                this.data[index] = value;
-                return index;
-            }
-
-            [MethodImpl(MethodImplOptions.AggressiveInlining)]
-            public void AddListRange(FastList<T> other) {
-                if (other.length > 0) {
-                    var newSize = this.length + other.length;
-                    if (newSize > this.capacity) {
-                        while (newSize > this.capacity) {
-                            this.capacity <<= 1;
-                        }
-
-                        ArrayHelpers.Grow(ref this.data, this.capacity);
-                    }
-
-                    if (this == other) {
-                        Array.Copy(this.data, 0, this.data, this.length, this.length);
-                    }
-                    else {
-                        Array.Copy(other.data, 0, this.data, this.length, other.length);
-                    }
-
-                    this.length += other.length;
-                }
-            }
-
-            [MethodImpl(MethodImplOptions.AggressiveInlining)]
-            public void Swap(int source, int destination) => this.data[destination] = this.data[source];
-
-            [MethodImpl(MethodImplOptions.AggressiveInlining)]
-            public int IndexOf(T value) => ArrayHelpers.IndexOf(this.data, value, this.comparer);
-
-            [MethodImpl(MethodImplOptions.AggressiveInlining)]
-            public void Remove(T value) => this.RemoveAt(this.IndexOf(value));
-
-            [MethodImpl(MethodImplOptions.AggressiveInlining)]
-            public void RemoveSwap(T value, out ResultSwap swap) => this.RemoveAtSwap(this.IndexOf(value), out swap);
-
-            [MethodImpl(MethodImplOptions.AggressiveInlining)]
-            public void RemoveAt(int index) {
-                --this.length;
-                if (index < this.length) {
-                    Array.Copy(this.data, index + 1, this.data, index, this.length - index);
-                }
-
-                this.data[this.length] = default;
-            }
-
-            [MethodImpl(MethodImplOptions.AggressiveInlining)]
-            public bool RemoveAtSwap(int index, out ResultSwap swap) {
-                if (this.length-- > 1) {
-                    swap.oldIndex = this.length;
-                    swap.newIndex = index;
-
-                    this.data[swap.newIndex] = this.data[swap.oldIndex];
-                    return true;
-                }
-
-                swap = default;
-                return false;
-            }
-
-            [MethodImpl(MethodImplOptions.AggressiveInlining)]
-            public void Clear() {
-                if (this.length <= 0) {
-                    return;
-                }
-
-                Array.Clear(this.data, 0, this.length);
-                this.length = 0;
-            }
-
-            //todo rework
-            [MethodImpl(MethodImplOptions.AggressiveInlining)]
-            public void Sort() => Array.Sort(this.data, 0, this.length, null);
-
-            //todo rework
-            [MethodImpl(MethodImplOptions.AggressiveInlining)]
-            public void Sort(int index, int len) => Array.Sort(this.data, index, len, null);
-
-            [MethodImpl(MethodImplOptions.AggressiveInlining)]
-            public T[] ToArray() {
-                var newArray = new T[this.length];
-                Array.Copy(this.data, 0, newArray, 0, this.length);
-                return newArray;
             }
 
             [MethodImpl(MethodImplOptions.AggressiveInlining)]
@@ -3425,6 +3321,116 @@ namespace Morpeh {
 
                 public void Dispose() {
                 }
+            }
+        }
+
+        [Il2Cpp(Option.NullChecks, false)]
+        [Il2Cpp(Option.ArrayBoundsChecks, false)]
+        [Il2Cpp(Option.DivideByZeroChecks, false)]
+        public static class FastListExtensions {
+            [MethodImpl(MethodImplOptions.AggressiveInlining)]
+            public static int Add<T>(this FastList<T> list) {
+                var index = list.length;
+                if (++list.length == list.capacity) {
+                    ArrayHelpers.Grow(ref list.data, list.capacity <<= 1);
+                }
+
+                return index;
+            }
+
+            [MethodImpl(MethodImplOptions.AggressiveInlining)]
+            public static int Add<T>(this FastList<T> list, T value) {
+                var index = list.length;
+                if (++list.length == list.capacity) {
+                    ArrayHelpers.Grow(ref list.data, list.capacity <<= 1);
+                }
+
+                list.data[index] = value;
+                return index;
+            }
+
+            [MethodImpl(MethodImplOptions.AggressiveInlining)]
+            public static void AddListRange<T>(this FastList<T> list, FastList<T> other) {
+                if (other.length > 0) {
+                    var newSize = list.length + other.length;
+                    if (newSize > list.capacity) {
+                        while (newSize > list.capacity) {
+                            list.capacity <<= 1;
+                        }
+
+                        ArrayHelpers.Grow(ref list.data, list.capacity);
+                    }
+
+                    if (list == other) {
+                        Array.Copy(list.data, 0, list.data, list.length, list.length);
+                    }
+                    else {
+                        Array.Copy(other.data, 0, list.data, list.length, other.length);
+                    }
+
+                    list.length += other.length;
+                }
+            }
+
+            [MethodImpl(MethodImplOptions.AggressiveInlining)]
+            public static void Swap<T>(this FastList<T> list, int source, int destination) => list.data[destination] = list.data[source];
+
+            [MethodImpl(MethodImplOptions.AggressiveInlining)]
+            public static int IndexOf<T>(this FastList<T> list, T value) => ArrayHelpers.IndexOf(list.data, value, list.comparer);
+
+            [MethodImpl(MethodImplOptions.AggressiveInlining)]
+            public static void Remove<T>(this FastList<T> list, T value) => list.RemoveAt(list.IndexOf(value));
+
+            [MethodImpl(MethodImplOptions.AggressiveInlining)]
+            public static void RemoveSwap<T>(this FastList<T> list, T value, out FastList<T>.ResultSwap swap) => list.RemoveAtSwap(list.IndexOf(value), out swap);
+
+            [MethodImpl(MethodImplOptions.AggressiveInlining)]
+            public static void RemoveAt<T>(this FastList<T> list, int index) {
+                --list.length;
+                if (index < list.length) {
+                    Array.Copy(list.data, index + 1, list.data, index, list.length - index);
+                }
+
+                list.data[list.length] = default;
+            }
+
+            [MethodImpl(MethodImplOptions.AggressiveInlining)]
+            public static bool RemoveAtSwap<T>(this FastList<T> list, int index, out FastList<T>.ResultSwap swap) {
+                if (list.length-- > 1) {
+                    swap.oldIndex = list.length;
+                    swap.newIndex = index;
+
+                    list.data[swap.newIndex] = list.data[swap.oldIndex];
+                    return true;
+                }
+
+                swap = default;
+                return false;
+            }
+
+            [MethodImpl(MethodImplOptions.AggressiveInlining)]
+            public static void Clear<T>(this FastList<T> list) {
+                if (list.length <= 0) {
+                    return;
+                }
+
+                Array.Clear(list.data, 0, list.length);
+                list.length = 0;
+            }
+
+            //todo rework
+            [MethodImpl(MethodImplOptions.AggressiveInlining)]
+            public static void Sort<T>(this FastList<T> list) => Array.Sort(list.data, 0, list.length, null);
+
+            //todo rework
+            [MethodImpl(MethodImplOptions.AggressiveInlining)]
+            public static void Sort<T>(this FastList<T> list, int index, int len) => Array.Sort(list.data, index, len, null);
+
+            [MethodImpl(MethodImplOptions.AggressiveInlining)]
+            public static T[] ToArray<T>(this FastList<T> list) {
+                var newArray = new T[list.length];
+                Array.Copy(list.data, 0, newArray, 0, list.length);
+                return newArray;
             }
         }
 
