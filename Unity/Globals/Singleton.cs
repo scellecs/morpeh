d69515b591fcd065cbb87b650a1f95daa788e971
Oleg Morozov <benjminmoore@gmail.com>
2020-08-20 16:56:46 +0300

    Fix Singleton

--- Unity/Globals/Singleton.cs
+++ Unity/Globals/Singleton.cs
@@ -22,13 +22,16 @@
 
         protected override bool CheckIsInitialized() {
             var check = base.CheckIsInitialized();
-            foreach (var component in this.serializedComponents) {
-                var type = component.GetType();
-                if (CommonTypeIdentifier.typeAssociation.TryGetValue(type, out var definition)) {
-                    definition.entitySetBoxed(this.internalEntity, component);
-                }
-                else {
-                    Debug.LogError($"[MORPEH] For using {type.Name} in a Singleton you must warmup it or IL2CPP will strip it from the build.\nCall <b>TypeIdentifier<{type.Name}>.Warmup();</b> before access this Singleton.");
+            if (check) {
+                foreach (var component in this.serializedComponents) {
+                    var type = component.GetType();
+                    if (CommonTypeIdentifier.typeAssociation.TryGetValue(type, out var definition)) {
+                        definition.entitySetBoxed(this.internalEntity, component);
+                    }
+                    else {
+                        Debug.LogError(
+                            $"[MORPEH] For using {type.Name} in a Singleton you must warmup it or IL2CPP will strip it from the build.\nCall <b>TypeIdentifier<{type.Name}>.Warmup();</b> before access this Singleton.");
+                    }
                 }
             }
             return check;
