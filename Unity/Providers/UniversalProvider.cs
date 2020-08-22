54144c0f8162cd642444109eb985dd3eafea00af
Oleg Morozov <benjminmoore@gmail.com>
2020-08-22 03:36:32 +0300

    Universal Provider++

---
+++ Unity/Providers/UniversalProvider.cs
@@ -0,0 +1,52 @@
+﻿namespace Morpeh {
+#if UNITY_EDITOR && ODIN_INSPECTOR
+    using Sirenix.OdinInspector;
+#endif
+    using UnityEngine;
+
+    public class UniversalProvider : EntityProvider {
+        [Space]
+        [SerializeReference]
+#if UNITY_EDITOR && ODIN_INSPECTOR
+        [HideIf(nameof(ShowSerializedComponents))]
+#endif
+        public IComponent[] serializedComponents = new IComponent[0];
+        
+#if UNITY_EDITOR && ODIN_INSPECTOR
+        private bool ShowSerializedComponents => this.internalEntityID > -1;
+#endif
+        
+        protected sealed override void PreInitialize() {
+            var ent = this.Entity;
+            if (ent != null && !ent.isDisposed) {
+                foreach (var component in this.serializedComponents) {
+                    var type = component.GetType();
+                    if (CommonTypeIdentifier.typeAssociation.TryGetValue(type, out var definition)) {
+                        definition.entitySetComponentBoxed(ent, component);
+                    }
+                    else {
+                        Debug.LogError(
+                            $"[MORPEH] For using {type.Name} in a UniversalProvider you must warmup it or IL2CPP will strip it from the build.\nCall <b>TypeIdentifier<{type.Name}>.Warmup();</b> before access this UniversalProvider.");
+                    }
+                }
+            }
+        }
+
+        protected override void OnDisable() {
+            var ent = this.Entity;
+            if (ent.IsNullOrDisposed() == false) {
+                foreach (var component in this.serializedComponents) {
+                    var type = component.GetType();
+                    if (CommonTypeIdentifier.typeAssociation.TryGetValue(type, out var definition)) {
+                        definition.entityRemoveComponent(ent);
+                    }
+                    else {
+                        Debug.LogError(
+                            $"[MORPEH] For using {type.Name} in a UniversalProvider you must warmup it or IL2CPP will strip it from the build.\nCall <b>TypeIdentifier<{type.Name}>.Warmup();</b> before access this UniversalProvider.");
+                    }
+                }
+            }
+            base.OnDisable();
+        }
+    }
+}
