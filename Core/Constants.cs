9900a7d1ba228217204b96145f7999d6ea314834
Oleg Morozov <benjminmoore@gmail.com>
2022-01-14 00:08:40 +0300

    Remove legacy constant

--- Core/Constants.cs
+++ Core/Constants.cs
@@ -2,7 +2,6 @@ namespace Morpeh {
     internal static class Constants {
         internal const int DEFAULT_WORLD_ENTITIES_CAPACITY    = 256;
         internal const int DEFAULT_WORLD_CACHES_CAPACITY      = 12;
-        internal const int DEFAULT_ENTITY_COMPONENTS_CAPACITY = 2;
         internal const int DEFAULT_CACHE_COMPONENTS_CAPACITY  = 256;
     }
 }
