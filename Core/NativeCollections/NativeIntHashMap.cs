0f4075db049f052a5de41c4a49d8786601d1b540
Igor Boyko <evolutionv8@yandex.ru>
2022-01-27 15:12:06 +0300

    Make NativeIntHashMap enumerable

--- Core/NativeCollections/NativeIntHashMap.cs
+++ Core/NativeCollections/NativeIntHashMap.cs
@@ -1,12 +1,14 @@
 ﻿#if UNITY_2019_1_OR_NEWER
 namespace morpeh.Core.Collections {
-    using Morpeh;
+    using System.Collections;
+    using System.Collections.Generic;
     using Morpeh.Collections;
     using Unity.Collections;
     using Unity.Collections.LowLevel.Unsafe;
+    using Unity.IL2CPP.CompilerServices;
 
     [NativeContainer]
-    public unsafe struct NativeIntHashMap<TNative> where TNative : unmanaged {
+    public unsafe struct NativeIntHashMap<TNative> : IEnumerable<int> where TNative : unmanaged {
         public int* lengthPtr;
         public int* capacityPtr;
         public int* capacityMinusOnePtr;
@@ -16,6 +18,50 @@ namespace morpeh.Core.Collections {
         public NativeArray<int>     buckets;
         public NativeArray<Slot>    slots;
         public NativeArray<TNative> data;
+        
+        IEnumerator<int> IEnumerable<int>.GetEnumerator() => this.GetEnumerator();
+
+        IEnumerator IEnumerable.GetEnumerator() => this.GetEnumerator();
+
+        [Il2CppSetOption(Option.NullChecks, false)]
+        [Il2CppSetOption(Option.ArrayBoundsChecks, false)]
+        [Il2CppSetOption(Option.DivideByZeroChecks, false)]
+        public struct Enumerator : IEnumerator<int> {
+            public NativeIntHashMap<TNative> hashMap;
+
+            public int index;
+            public int current;
+
+            public bool MoveNext() {
+                for (; this.index < *this.hashMap.lastIndexPtr; ++this.index) {
+                    var slot = this.hashMap.slots[this.index];
+                    if (slot.key - 1 < 0) {
+                        continue;
+                    }
+
+                    this.current = this.index;
+                    ++this.index;
+
+                    return true;
+                }
+
+                this.index   = *this.hashMap.lastIndexPtr + 1;
+                this.current = default;
+                return false;
+            }
+
+            public int Current => this.current;
+
+            object IEnumerator.Current => this.current;
+
+            void IEnumerator.Reset() {
+                this.index   = 0;
+                this.current = default;
+            }
+
+            public void Dispose() {
+            }
+        }
     }
 }
 #endif
