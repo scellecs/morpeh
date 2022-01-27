81fcdbca9c9152765aa58101540291fb8670fe3d
Oleg Morozov <benjminmoore@gmail.com>
2022-01-27 11:45:32 +0300

    Fix #93

--- Core/Collections/BitMapExtensions.cs
+++ Core/Collections/BitMapExtensions.cs
@@ -180,11 +180,42 @@ namespace Morpeh.Collections {
         }
 
         [MethodImpl(MethodImplOptions.AggressiveInlining)]
-        public static int NumberOfTrailingZeros(int i) => lookupTrailingZeros[((uint)(i & -i) * 0x077CB531u) >> 27];
-
-        private static readonly int[] lookupTrailingZeros = {
-            0, 1, 28, 2, 29, 14, 24, 3, 30, 22, 20, 15, 25, 17, 4, 8,
-            31, 27, 13, 23, 21, 19, 16, 7, 26, 12, 18, 6, 11, 5, 10, 9
-        };
+        public static int NumberOfTrailingZeros(int i) {
+            switch (((uint)(i & -i) * 0x077CB531u) >> 27) {
+                case 0:  return 0;
+                case 1:  return 1;
+                case 2:  return 28;
+                case 3:  return 2;
+                case 4:  return 29;
+                case 5:  return 14;
+                case 6:  return 24;
+                case 7:  return 3;
+                case 8:  return 30;
+                case 9:  return 22;
+                case 10: return 20;
+                case 11: return 15;
+                case 12: return 25;
+                case 13: return 17;
+                case 14: return 4;
+                case 15: return 8;
+                case 16: return 31;
+                case 17: return 27;
+                case 18: return 13;
+                case 19: return 23;
+                case 20: return 21;
+                case 21: return 19;
+                case 22: return 16;
+                case 23: return 7;
+                case 24: return 26;
+                case 25: return 12;
+                case 26: return 18;
+                case 27: return 6;
+                case 28: return 11;
+                case 29: return 5;
+                case 30: return 10;
+                case 31: return 9;
+            }
+            return 0;
+        }
     }
 }
