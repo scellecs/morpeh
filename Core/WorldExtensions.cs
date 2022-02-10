cf25ca6673acb46783b49f3b7c59573d3e64dace
Igor Boyko <evolutionv8@yandex.ru>
2022-02-10 18:59:40 +0300

    Fix strange behaviour of transfers (revert dirtyEntities to bitmap)

--- Core/WorldExtensions.cs
+++ Core/WorldExtensions.cs
@@ -27,7 +27,7 @@ namespace Morpeh {
             world.Filter         = new Filter(world);
             world.filters        = new FastList<Filter>();
             world.archetypeCache = new IntFastList();
-            world.dirtyEntities  = new FastList<int>();
+            world.dirtyEntities  = new BitMap();
 
             if (world.archetypes != null) {
                 foreach (var archetype in world.archetypes) {
