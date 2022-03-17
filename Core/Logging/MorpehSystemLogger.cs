d7117642693b6f03c58230567def58046f3fe340
Igor Boyko <evolutionv8@yandex.ru>
2022-03-17 14:41:45 +0300

    Allow custom loggers

---
+++ Core/Logging/MorpehSystemLogger.cs
@@ -0,0 +1,10 @@
+﻿namespace Morpeh.Logging
+{
+    internal class MorpehSystemLogger : IMorpehLogger
+    {
+        public void Log(string message) => System.Console.WriteLine(message);
+        public void LogWarning(string message) => System.Console.WriteLine(message);
+        public void LogError(string message) => System.Console.WriteLine(message);
+        public void LogException(System.Exception exception) => System.Console.WriteLine(exception);
+    }
+}
---
+++ Core/MorpehSettings.cs
@@ -0,0 +1,17 @@
+﻿namespace Morpeh
+{
+    using Logging;
+    
+    public static class MorpehSettings
+    {
+#if UNITY_2019_1_OR_NEWER
+        internal static IMorpehLogger Logger = new MorpehUnityLogger();
+#else
+        internal static IMorpehLogger Logger = new MorpehSystemLogger();
+#endif
+        
+        public static void UseLogger(IMorpehLogger logger) {
+            Logger = logger;
+        }
+    }
+}
