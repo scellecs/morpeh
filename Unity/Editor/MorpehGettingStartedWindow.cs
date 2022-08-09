18c9b9ccbcc50c2abbc50c4b6cce824d30384730
VladV <vanifatov.vlad@yandex.ru>
2022-08-09 12:48:46 +0300

    Add GettingStarted window

---
+++ Unity/Editor/MorpehGettingStartedWindow.cs
@@ -0,0 +1,107 @@
+﻿namespace Morpeh.Editor {
+    using System;
+    using System.Collections.Generic;
+    using System.Linq;
+    using UnityEditor;
+    using UnityEngine;
+
+    internal class MorpehGettingStartedWindow : EditorWindow {
+        [InitializeOnLoadMethod]
+        public static void ShowIfRequired() {
+            var targetGroup = EditorUserBuildSettings.selectedBuildTargetGroup;
+            var defines     = PlayerSettings.GetScriptingDefineSymbolsForGroup(targetGroup);
+
+            if ($";{defines};".Contains(";MORPEH;")) {
+                return;
+            }
+
+            EditorApplication.delayCall += Open;
+        }
+
+        [MenuItem("Tools/Morpeh/Getting Started")]
+        public static void Open() {
+            var window = GetWindow<MorpehGettingStartedWindow>();
+            window.titleContent = new GUIContent("Morpeh");
+            window.Show();
+        }
+
+        [NonSerialized] private Vector2 scroll;
+
+
+        private void OnEnable() {
+            if (!MorpehModules.CurrentDefines.Contains(MorpehModules.MORPEH_DEFINE)) {
+                var definesToAdd = new List<string> {
+                    MorpehModules.MORPEH_DEFINE,
+                };
+
+                foreach (var moduleInfo in MorpehModules.Modules) {
+                    if (moduleInfo.autoEnable) {
+                        definesToAdd.AddRange(MorpehModules.EnumerateModuleDefinesRecursive(moduleInfo));
+                    }
+                }
+
+                MorpehModules.AddDefineSymbols(definesToAdd);
+            }
+        }
+
+        private void OnGUI() {
+            this.DrawHeader();
+
+            using (new EditorGUI.DisabledScope(EditorApplication.isCompiling))
+            using (var scrollScope = new GUILayout.ScrollViewScope(this.scroll, false, true)) {
+                this.scroll = scrollScope.scrollPosition;
+
+                this.DrawModules();
+            }
+        }
+
+        private void DrawHeader() {
+            using (new GUILayout.VerticalScope(Styles.BigTitle)) {
+                GUILayout.Label("Morpeh", Styles.HeaderLabel);
+                GUILayout.Label("ECS Framework for Unity Game Engine");
+            }
+        }
+
+        private void DrawModules() {
+            using (new GUILayout.VerticalScope(Styles.BigTitle)) {
+                GUILayout.Label("Installed Modules", EditorStyles.largeLabel);
+
+                foreach (var moduleInfo in MorpehModules.Modules) {
+                    using (new GUILayout.HorizontalScope(GUI.skin.box)) {
+                        GUILayout.Label(moduleInfo.name, EditorStyles.largeLabel);
+
+                        GUILayout.FlexibleSpace();
+
+                        var isModuleEnabled = MorpehModules.IsModuleEnabled(moduleInfo);
+                        var canDisable      = MorpehModules.CanDisableModule(moduleInfo, out var disableLockReason);
+
+                        using (new EditorGUI.DisabledScope(!canDisable)) {
+                            var toggleContent = new GUIContent("", disableLockReason);
+
+                            if (GUILayout.Toggle(isModuleEnabled, toggleContent) != isModuleEnabled) {
+                                if (isModuleEnabled) {
+                                    MorpehModules.RemoveDefineSymbols(new[] {moduleInfo.define});
+                                }
+                                else {
+                                    MorpehModules.AddDefineSymbols(MorpehModules.EnumerateModuleDefinesRecursive(moduleInfo));
+                                }
+                            }
+                        }
+                    }
+                }
+            }
+        }
+
+        private static class Styles {
+            public static readonly GUIStyle HeaderLabel;
+            public static readonly GUIStyle BigTitle;
+
+            static Styles() {
+                HeaderLabel = new GUIStyle(EditorStyles.largeLabel) {
+                    fontSize = 24,
+                };
+                BigTitle = new GUIStyle("IN BigTitle");
+            }
+        }
+    }
+}
---
+++ Unity/Editor/MorpehModules.cs
@@ -0,0 +1,123 @@
+﻿namespace Morpeh.Editor {
+    using System;
+    using System.Collections.Generic;
+    using System.Linq;
+    using UnityEditor;
+    using UnityEditor.Callbacks;
+
+    internal static class MorpehModules {
+        public const string MORPEH_DEFINE = "MORPEH";
+
+        public static BuildTargetGroup CurrentTargetGroup;
+        public static string[]         CurrentDefines;
+
+        [InitializeOnLoadMethod]
+        [DidReloadScripts]
+        public static void Refresh() {
+            CurrentTargetGroup = EditorUserBuildSettings.selectedBuildTargetGroup;
+            CurrentDefines     = PlayerSettings.GetScriptingDefineSymbolsForGroup(CurrentTargetGroup).Split(';');
+        }
+
+        public static readonly MorpehModuleInfo[] Modules = {
+            new MorpehModuleInfo {
+                name       = "Morpeh.Globals",
+                define     = "MORPEH_GLOBALS",
+                autoEnable = true,
+            },
+            new MorpehModuleInfo {
+                name       = "Morpeh.Systems",
+                define     = "MORPEH_SYSTEMS",
+                autoEnable = true,
+            },
+            new MorpehModuleInfo {
+                name         = "Morpeh.Installers",
+                define       = "MORPEH_INSTALLERS",
+                dependencies = new[] {"Morpeh.Systems"},
+                autoEnable   = true,
+            },
+            new MorpehModuleInfo {
+                name       = "Morpeh.Providers",
+                define     = "MORPEH_PROVIDERS",
+                autoEnable = true,
+            },
+            new MorpehModuleInfo {
+                name         = "Morpeh.Utils",
+                define       = "MORPEH_UTILS",
+                dependencies = new[] {"Morpeh.Globals", "Morpeh.Systems", "Morpeh.Installers", "Morpeh.Providers"},
+            },
+        };
+
+        public static bool IsModuleEnabled(MorpehModuleInfo moduleInfo) {
+            return CurrentDefines.Contains(moduleInfo.define);
+        }
+
+        public static bool CanDisableModule(MorpehModuleInfo moduleInfo, out string reason) {
+            reason = null;
+
+            foreach (var other in Modules) {
+                if (IsModuleEnabled(other) && other.dependencies.Contains(moduleInfo.name)) {
+                    if (reason != null) {
+                        reason += Environment.NewLine;
+                    }
+
+                    reason += $"Required by {other.name}";
+                }
+            }
+
+            return reason == null;
+        }
+
+        public static MorpehModuleInfo FindModule(string moduleName) {
+            foreach (var moduleInfo in Modules) {
+                if (moduleInfo.name == moduleName) {
+                    return moduleInfo;
+                }
+            }
+
+            return null;
+        }
+
+        public static IEnumerable<string> EnumerateModuleDefinesRecursive(MorpehModuleInfo moduleInfo) {
+            yield return moduleInfo.define;
+
+            foreach (var dependencyModuleName in moduleInfo.dependencies) {
+                var dependencyModule = FindModule(dependencyModuleName);
+                foreach (var define in EnumerateModuleDefinesRecursive(dependencyModule)) {
+                    yield return define;
+                }
+            }
+        }
+
+        public static void AddDefineSymbols(IEnumerable<string> definesToAdd) {
+            var newDefines = new List<string>(CurrentDefines);
+
+            foreach (var define in definesToAdd) {
+                if (!newDefines.Contains(define)) {
+                    newDefines.Add(define);
+                }
+            }
+
+            var newDefinesString = string.Join(";", newDefines.ToArray());
+            PlayerSettings.SetScriptingDefineSymbolsForGroup(CurrentTargetGroup, newDefinesString);
+        }
+
+        public static void RemoveDefineSymbols(IEnumerable<string> definesToRemove) {
+            var newDefines = new List<string>(CurrentDefines);
+
+            foreach (var define in definesToRemove) {
+                newDefines.Remove(define);
+            }
+
+            var newDefinesString = string.Join(";", newDefines.ToArray());
+            PlayerSettings.SetScriptingDefineSymbolsForGroup(CurrentTargetGroup, newDefinesString);
+        }
+
+        [Serializable]
+        public class MorpehModuleInfo {
+            public string   name;
+            public string   define;
+            public string[] dependencies = Array.Empty<string>();
+            public bool     autoEnable;
+        }
+    }
+}
