946a6dc8bba45336ff0973c201e746e5fc7c4675
Oleg Morozov <benjminmoore@gmail.com>
2023-02-21 17:30:14 +0300

    🚧 Rework pinned array

--- Core/Collections/Unsafe/PinnedArray.cs
+++ Core/Collections/Unsafe/PinnedArray.cs
@@ -1,9 +1,17 @@
+#if ENABLE_MONO || ENABLE_IL2CPP
+#define MORPEH_UNITY
+#endif
+
 namespace Scellecs.Morpeh.Collections {
     using System;
     using System.Collections;
     using System.Collections.Generic;
     using System.Runtime.CompilerServices;
+#if MORPEH_UNITY
+    using Unity.Collections.LowLevel.Unsafe;
+#else
     using System.Runtime.InteropServices;
+#endif
     using Unity.IL2CPP.CompilerServices;
 
     [Serializable]
@@ -12,25 +20,41 @@ namespace Scellecs.Morpeh.Collections {
     [Il2CppSetOption(Option.DivideByZeroChecks, false)]
     public unsafe struct PinnedArray<T> : IDisposable, IEnumerable<T> where T : unmanaged {
         public T[] data;
-        public GCHandle handle;
         public T* ptr;
+#if MORPEH_UNITY
+        public ulong handle;
+#else
+        public GCHandle handle;
+#endif
 
         [MethodImpl(MethodImplOptions.AggressiveInlining)]
         public PinnedArray(int size) {
             this.data = new T[size];
+#if MORPEH_UNITY
+            this.ptr = (T*) UnsafeUtility.PinGCArrayAndGetDataAddress(this.data, out this.handle);
+#else
             this.handle = GCHandle.Alloc(this.data, GCHandleType.Pinned);
             this.ptr = (T*)this.handle.AddrOfPinnedObject();
+#endif
         }
 
         [MethodImpl(MethodImplOptions.AggressiveInlining)]
         public void Resize(int newSize) {
+#if MORPEH_UNITY
+            UnsafeUtility.ReleaseGCObject(this.handle);
+#else
             this.handle.Free();
+#endif
             var newArray = new T[newSize];
             var len = this.data.Length;
             Array.Copy(this.data, 0, newArray, 0, newSize >= len ? len : newSize);
             this.data = newArray;
+#if MORPEH_UNITY
+            this.ptr = (T*) UnsafeUtility.PinGCArrayAndGetDataAddress(this.data, out this.handle);
+#else
             this.handle = GCHandle.Alloc(newArray, GCHandleType.Pinned);
             this.ptr = (T*)this.handle.AddrOfPinnedObject();
+#endif
         }
 
         [MethodImpl(MethodImplOptions.AggressiveInlining)]
@@ -40,11 +64,17 @@ namespace Scellecs.Morpeh.Collections {
         
         [MethodImpl(MethodImplOptions.AggressiveInlining)]
         public void Dispose() {
+#if MORPEH_UNITY
+            UnsafeUtility.ReleaseGCObject(this.handle);
+            this.ptr = (T*)IntPtr.Zero;
+            this.data = null;
+#else
             if (this.handle.IsAllocated) {
                 this.handle.Free();
                 this.ptr = (T*)IntPtr.Zero;
                 this.data = null;
             }
+#endif
         }
         [MethodImpl(MethodImplOptions.AggressiveInlining)]
         public Enumerator GetEnumerator() {
