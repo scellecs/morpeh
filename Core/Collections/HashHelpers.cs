898cf700631f0f90a929a27d0207a04980d4d372
Gallardo994 <evolutionv8@yandex.ru>
2023-04-21 02:35:44 +0400

    Update HashHelpers.cs

--- Core/Collections/HashHelpers.cs
+++ Core/Collections/HashHelpers.cs
@@ -3,6 +3,7 @@ namespace Scellecs.Morpeh.Collections {
     using System.Runtime.CompilerServices;
     using Unity.IL2CPP.CompilerServices;
 
+    [Il2CppEagerStaticClassConstruction]
     [Il2CppSetOption(Option.NullChecks, false)]
     [Il2CppSetOption(Option.ArrayBoundsChecks, false)]
     [Il2CppSetOption(Option.DivideByZeroChecks, false)]
