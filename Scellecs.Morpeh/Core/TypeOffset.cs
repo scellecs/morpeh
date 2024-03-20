77d1b84c69fd36f80ecdc0f444219e0683f7be9b
Gallardo994 <git@gx994.net>
2024-03-20 16:26:32 +0400

    Restore equality & hashcode for type id and offset

--- Scellecs.Morpeh/Core/TypeOffset.cs
+++ Scellecs.Morpeh/Core/TypeOffset.cs
@@ -6,7 +6,7 @@
     [Il2CppSetOption(Option.NullChecks, false)]
     [Il2CppSetOption(Option.ArrayBoundsChecks, false)]
     [Il2CppSetOption(Option.DivideByZeroChecks, false)]
-    public readonly struct TypeOffset {
+    public readonly struct TypeOffset : IEquatable<TypeOffset> {
         private readonly int value;
         
         public TypeOffset(int value) {
@@ -17,6 +17,16 @@
         public int GetValue() {
             return this.value;
         }
+        
+        [MethodImpl(MethodImplOptions.AggressiveInlining)]
+        public bool Equals(TypeOffset other) {
+            return value == other.value;
+        }
+
+        [MethodImpl(MethodImplOptions.AggressiveInlining)]
+        public override bool Equals(object obj) {
+            return obj is TypeId other && Equals(other);
+        }
 
         [MethodImpl(MethodImplOptions.AggressiveInlining)]
         public static bool operator ==(TypeOffset a, TypeOffset b) {
@@ -26,6 +36,11 @@
         [MethodImpl(MethodImplOptions.AggressiveInlining)]
         public static bool operator !=(TypeOffset a, TypeOffset b) {
             return a.value != b.value;
+        }
+        
+        [MethodImpl(MethodImplOptions.AggressiveInlining)]
+        public override int GetHashCode() {
+            return value.GetHashCode();
         }
         
         [MethodImpl(MethodImplOptions.AggressiveInlining)]
