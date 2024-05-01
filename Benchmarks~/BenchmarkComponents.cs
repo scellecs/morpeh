8b951c60c33710e83a00b8ecdf2c65897706c391
Oleg Morozov <benjminmoore@gmail.com>
2024-05-01 17:38:53 +0300

    🚧 Iteration Bench

--- Benchmarks~/BenchmarkComponents.cs
+++ Benchmarks~/BenchmarkComponents.cs
@@ -2,7 +2,18 @@
 
 namespace Benchmarks;
 
-public struct Test1 : IComponent { }
-public struct Test2 : IComponent { }
-public struct Test3 : IComponent { }
-public struct Test4 : IComponent { }
+public struct Test1 : IComponent {
+    public int value;
+}
+
+public struct Test2 : IComponent {
+    public int value;
+}
+
+public struct Test3 : IComponent {
+    public int value;
+}
+
+public struct Test4 : IComponent {
+    public int value;
+}
