57dbf19a7011b8adcc3de5519f8735a3bcc29669
Gallardo994 <git@gx994.net>
2024-10-03 16:36:25 +0400

    Basic tests for stash leaks

--- Tests~/TestComponents.cs
+++ Tests~/TestComponents.cs
@@ -6,3 +6,5 @@ public struct Test1 : IComponent { }
 public struct Test2 : IComponent { }
 public struct Test3 : IComponent { }
 public struct Test4 : IComponent { }
+
+public struct IntTest1 : IComponent { public int value; }
