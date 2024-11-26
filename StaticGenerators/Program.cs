5b2c94b20c6813af25a0ca3f940e9008a811a3c3
Gallardo994 <git@gx994.net>
2024-11-26 22:05:50 +0400

    Basic static generators

---
+++ StaticGenerators/Program.cs
@@ -0,0 +1,21 @@
+﻿// PinnedArray<Entity> -> PinnedArrayEntity
+
+using StaticGenerators;
+
+new PinnedArrayGenerator(config => {
+    config.ClassName          = "IntPinnedArray";
+    config.DataType           = "int";
+    config.Namespace          = "Scellecs.Morpeh.Collections";
+    config.ExtraUsings        = null;
+    config.GenerateEnumerator = true;
+}).Run("Scellecs.Morpeh/Core/Collections/Unsafe/IntPinnedArray.cs");
+
+/*
+new PinnedArrayGenerator(config => {
+    config.ClassName          = "PinnedArrayEntity";
+    config.DataType           = "Entity";
+    config.Namespace          = "Scellecs.Morpeh.Collections";
+    config.ExtraUsings        = null;
+    config.GenerateEnumerator = false;
+}).Run("Scellecs.Morpeh/Core/Archetypes/PinnedArrayEntity.cs");
+*/
