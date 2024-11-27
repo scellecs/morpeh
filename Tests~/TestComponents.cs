c9f6ef394ced249051c2a308c03ba96c07d85dd4
Gallardo994 <git@gx994.net>
2024-11-27 17:32:07 +0400

    Add filter match test for lots of filters (resize test)

--- Tests~/TestComponents.cs
+++ Tests~/TestComponents.cs
@@ -6,6 +6,10 @@ public struct Test1 : IComponent { }
 public struct Test2 : IComponent { }
 public struct Test3 : IComponent { }
 public struct Test4 : IComponent { }
+public struct Test5 : IComponent { }
+public struct Test6 : IComponent { }
+public struct Test7 : IComponent { }
+public struct Test8 : IComponent { }
 
 public struct IntTest1 : IComponent { public int value; }
 
