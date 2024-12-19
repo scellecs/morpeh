197781776612977ffde308b475a58d9dac0cf501
Gallardo994 <git@gx994.net>
2024-12-19 09:51:00 +0400

    Check il2cpp only

--- SourceGenerators~/SourceGenerators/MorpehHelpers/NonSemantic/MorpehDefines.cs
+++ SourceGenerators~/SourceGenerators/MorpehHelpers/NonSemantic/MorpehDefines.cs
@@ -5,6 +5,7 @@
         public const string MORPEH_DEBUG                = "MORPEH_DEBUG";
         public const string MORPEH_PROFILING            = "MORPEH_PROFILING";
         public const string MORPEH_ENABLE_IL2CPP_CHECKS = "MORPEH_ENABLE_IL2CPP_CHECKS";
+        public const string ENABLE_IL2CPP               = "ENABLE_IL2CPP";
         public const string ENABLE_MONO_OR_IL2CPP       = "ENABLE_MONO || ENABLE_IL2CPP";
         
         public static StringBuilder AppendMorpehDebugDefines(this StringBuilder sb) {
