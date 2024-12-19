31ff3df93a4f6ecbd1bd7fc015b246b45657e938
Gallardo994 <git@gx994.net>
2024-12-19 10:06:00 +0400

    Delete unused helper

--- SourceGenerators~/SourceGenerators/MorpehHelpers/NonSemantic/MorpehComponentHelpers.cs
+++
@@ -1,8 +0,0 @@
-﻿namespace SourceGenerators.MorpehHelpers.NonSemantic {
-    public static class MorpehComponentHelpers {
-        public static string ComponentNameToMetadataClassName(string componentName) {
-            var index = componentName.IndexOf('<');
-            return index > 0 ? componentName.Insert(index, "__Metadata") : $"{componentName}__Metadata";
-        }
-    }
-}
