493ccc70bd8369e7b15636275354af976b1dceeb
Gallardo994 <git@gx994.net>
2024-12-20 14:25:14 +0400

    Add more providers just for testing

---
+++ SourceGenerators~/SourceGenerators.Fiddle/TagComponentProvider.cs
@@ -0,0 +1,9 @@
+﻿namespace SourceGenerators.Fiddle;
+
+using Scellecs.Morpeh.Providers;
+using Test.Namespace;
+
+[MonoProvider(typeof(TagComponent))]
+public partial class TagComponentProvider {
+    
+}
