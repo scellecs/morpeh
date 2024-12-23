5645996c0a6b8deac432ef1f9a45e7e2c9a50f3d
Gallardo994 <git@gx994.net>
2024-12-24 01:13:16 +0400

    Update benchmarks to use generated components

--- Benchmarks/dotnet~/BenchmarkComponents.cs
+++ Benchmarks/dotnet~/BenchmarkComponents.cs
@@ -2,18 +2,37 @@
 
 namespace Benchmarks;
 
-public struct Test1 : IComponent {
+[Component]
+public partial struct IntTest1 {
     public int value;
 }
 
-public struct Test2 : IComponent {
+[Component]
+public partial struct IntTest2 {
     public int value;
 }
 
-public struct Test3 : IComponent {
+[Component]
+public partial struct IntTest3 {
     public int value;
 }
 
-public struct Test4 : IComponent {
+[Component]
+public partial struct IntTest4 {
     public int value;
+}
+
+[Component]
+public partial struct TagTest1 {
+    
+}
+
+[Component]
+public partial struct TagTest2 {
+    
+}
+
+[Component]
+public partial struct TagTest3 {
+    
 }
--- Benchmarks/dotnet~/MigrationsBenchmarks.cs
+++ Benchmarks/dotnet~/MigrationsBenchmarks.cs
@@ -15,9 +15,12 @@ public class MigrationsBenchmarks {
 
     private World world;
     
-    private Stash<Test1> test1;
-    private Stash<Test2> test2;
-    private Stash<Test3> test3;
+    private Stash<IntTest1> intTest1;
+    private Stash<IntTest2> intTest2;
+    private Stash<IntTest3> intTest3;
+    
+    private TagStash tagTest2;
+    private TagStash tagTest3;
     
     private Filter filter;
     
@@ -27,63 +30,113 @@ public class MigrationsBenchmarks {
     public void SetUp() {
         this.world = World.Create();
         
-        this.test1 = this.world.GetStash<Test1>();
-        this.test2 = this.world.GetStash<Test2>();
-        this.test3 = this.world.GetStash<Test3>();
+        this.intTest1 = IntTest1.GetStash(this.world);
+        this.intTest2 = IntTest2.GetStash(this.world);
+        this.intTest3 = IntTest3.GetStash(this.world);
         
-        this.filter = this.world.Filter.With<Test1>().Build();
+        this.tagTest2 = TagTest2.GetStash(this.world);
+        this.tagTest3 = TagTest3.GetStash(this.world);
+        
+        this.filter = this.world.Filter.With<IntTest1>().Build();
         
         this.buffer = new Entity[Count];
         for (var i = 0; i < Count; i++) {
             this.buffer[i] = this.world.CreateEntity();
-            this.test1.Set(this.buffer[i], new Test1());
+            this.intTest1.Set(this.buffer[i], new IntTest1());
         }
         
         this.world.Commit();
     }
 
     // [Benchmark]
-    public void SetRemoveComponentArraySingle() {
+    public void DataStash_SetRemoveComponentArraySingle() {
         foreach (var entity in this.buffer) {
-            this.test2.Set(entity, new Test2());
+            this.intTest2.Set(entity, new IntTest2());
         }
         
         this.world.Commit();
         
         foreach (var entity in this.buffer) {
-            this.test2.Remove(entity);
+            this.intTest2.Remove(entity);
         }
         
         this.world.Commit();
     }
     
     // [Benchmark]
-    public void SetRemoveComponentFilterSingle() {
-        foreach (var entity in this.filter) {
-            this.test2.Set(entity, new Test2());
+    public void TagStash_SetRemoveComponentArraySingle() {
+        foreach (var entity in this.buffer) {
+            this.tagTest2.Set(entity);
         }
         
         this.world.Commit();
         
-        foreach (var entity in this.filter) {
-            this.test2.Remove(entity);
+        foreach (var entity in this.buffer) {
+            this.tagTest2.Remove(entity);
         }
         
         this.world.Commit();
     }
     
     // [Benchmark]
-    public void SetRemoveComponentFilterDouble() {
+    public void DataStash_SetRemoveComponentFilterSingle() {
         foreach (var entity in this.filter) {
-            this.test2.Set(entity, new Test2());
-            this.test3.Set(entity, new Test3());
+            this.intTest2.Set(entity, new IntTest2());
         }
         
         this.world.Commit();
         
         foreach (var entity in this.filter) {
-            this.test2.Remove(entity);
-            this.test3.Remove(entity);
+            this.intTest2.Remove(entity);
+        }
+        
+        this.world.Commit();
+    }
+    
+    // [Benchmark]
+    public void TagStash_SetRemoveComponentFilterSingle() {
+        foreach (var entity in this.filter) {
+            this.tagTest2.Set(entity);
+        }
+        
+        this.world.Commit();
+        
+        foreach (var entity in this.filter) {
+            this.tagTest2.Remove(entity);
+        }
+        
+        this.world.Commit();
+    }
+    
+    // [Benchmark]
+    public void DataStash_SetRemoveComponentFilterDouble() {
+        foreach (var entity in this.filter) {
+            this.intTest2.Set(entity, new IntTest2());
+            this.intTest3.Set(entity, new IntTest3());
+        }
+        
+        this.world.Commit();
+        
+        foreach (var entity in this.filter) {
+            this.intTest2.Remove(entity);
+            this.intTest3.Remove(entity);
+        }
+        
+        this.world.Commit();
+    }
+    
+    // [Benchmark]
+    public void TagStash_SetRemoveComponentFilterDouble() {
+        foreach (var entity in this.filter) {
+            this.tagTest2.Set(entity);
+            this.tagTest3.Set(entity);
+        }
+        
+        this.world.Commit();
+        
+        foreach (var entity in this.filter) {
+            this.tagTest2.Remove(entity);
+            this.tagTest3.Remove(entity);
         }
         
         this.world.Commit();
@@ -92,9 +145,9 @@ public class MigrationsBenchmarks {
     // [Benchmark]
     public void NewArchetypeFuckery() {
         foreach (var entity in this.buffer) {
-            this.test2.Set(entity, new Test2());
+            this.intTest2.Set(entity, new IntTest2());
             this.world.Commit();
-            this.test2.Remove(entity);
+            this.intTest2.Remove(entity);
             this.world.Commit();
         }
     }
