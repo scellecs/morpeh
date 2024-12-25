63fa1f64117510d5603615c4910345f486708750
Gallardo994 <git@gx994.net>
2024-12-25 15:57:46 +0400

    Boxed api extensions for world

---
+++ Scellecs.Morpeh/Core/Worlds/WorldBoxedApiExtensions.cs
@@ -0,0 +1,14 @@
+﻿// TODO: Remove TRUE after migrating functionality to the new API
+#if UNITY_EDITOR || MORPEH_ENABLE_RUNTIME_BOXING_API || TRUE
+
+namespace Scellecs.Morpeh {
+    using System;
+    
+    public static class WorldBoxedApiExtensions {
+        public static IComponent GetBoxed(this World world, Type type, Entity entity) => world.GetReflectionStash(type).GetBoxed(entity);
+        public static IComponent GetBoxed(this World world, Type type, Entity entity, out bool exists) => world.GetReflectionStash(type).GetBoxed(entity, out exists);
+        public static void       SetBoxed(this World world, Type type, Entity entity, IComponent value) => world.GetReflectionStash(type).SetBoxed(entity, value);
+    }
+}
+
+#endif
