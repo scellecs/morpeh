0a8c5d933a40a252f64461f8b79c4f60c73447d5
Gallardo994 <git@gx994.net>
2024-12-26 03:52:15 +0400

    NativeDisposableStash

---
+++ Scellecs.Morpeh/Core/Native/NativeDisposableStash.cs
@@ -0,0 +1,18 @@
+﻿using Unity.Collections;
+using Unity.Collections.LowLevel.Unsafe;
+
+namespace Scellecs.Morpeh.Native
+{
+    public struct NativeDisposableStash<TNative> where TNative : unmanaged, IDisposableComponent {
+        [NativeDisableParallelForRestriction]
+        [NativeDisableUnsafePtrRestriction]
+        public unsafe TNative* data;
+
+        [NativeDisableParallelForRestriction]
+        [NativeDisableUnsafePtrRestriction]
+        public unsafe TNative* empty;
+
+        public NativeIntSlotMap map;
+        public NativeWorld world;
+    }
+}
