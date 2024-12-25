df5eeba150e65ab2f3ff7e2afc4c1542a130f480
Gallardo994 <git@gx994.net>
2024-12-25 16:05:18 +0400

    Add more boxed api functionality

--- Scellecs.Morpeh/Core/Worlds/WorldBoxedApiExtensions.cs
+++ Scellecs.Morpeh/Core/Worlds/WorldBoxedApiExtensions.cs
@@ -7,7 +7,11 @@ namespace Scellecs.Morpeh {
     public static class WorldBoxedApiExtensions {
         public static IComponent GetBoxed(this World world, Type type, Entity entity) => world.GetReflectionStash(type).GetBoxed(entity);
         public static IComponent GetBoxed(this World world, Type type, Entity entity, out bool exists) => world.GetReflectionStash(type).GetBoxed(entity, out exists);
-        public static void       SetBoxed(this World world, Type type, Entity entity, IComponent value) => world.GetReflectionStash(type).SetBoxed(entity, value);
+        public static void SetBoxed(this World world, Type type, Entity entity, IComponent value) => world.GetReflectionStash(type).SetBoxed(entity, value);
+        public static void HasBoxed(this World world, Type type, Entity entity) => world.GetReflectionStash(type).Has(entity);
+        public static void RemoveBoxed(this World world, Type type, Entity entity) => world.GetReflectionStash(type).Remove(entity);
+        public static void RemoveAllBoxed(this World world, Type type) => world.GetReflectionStash(type).RemoveAll();
+        public static void MigrateBoxed(this World world, Type type, Entity from, Entity to) => world.GetReflectionStash(type).Migrate(from, to);
     }
 }
 
