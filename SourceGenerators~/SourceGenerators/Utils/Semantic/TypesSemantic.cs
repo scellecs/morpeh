aeb3898c5bdf82fc3787c84b8b525d3a60829080
Gallardo994 <git@gx994.net>
2025-01-08 02:25:47 +0400

    Remove method unless slow mode

--- SourceGenerators~/SourceGenerators/Utils/Semantic/TypesSemantic.cs
+++ SourceGenerators~/SourceGenerators/Utils/Semantic/TypesSemantic.cs
@@ -18,11 +18,7 @@
             return sb;
         }
         
-        /// <summary>
-        /// Check if the type contains fields with the specified attribute.
-        /// For original type, checks all fields.
-        /// For base types, checks only public and protected fields.
-        /// </summary>
+#if MORPEH_SOURCEGEN_INJECTABLE_SCAN_SLOW
         public static bool ContainsFieldsWithAttribute(ITypeSymbol typeSymbol, string attributeName) {
             var includePrivate = true;
             var currentSymbol = typeSymbol;
@@ -58,6 +54,7 @@
 
             return false;
         }
+#endif
         
         public static void FillFieldsWithAttribute(List<IFieldSymbol> symbols, ITypeSymbol typeSymbol, string attributeName) {
             var includePrivate = true;
