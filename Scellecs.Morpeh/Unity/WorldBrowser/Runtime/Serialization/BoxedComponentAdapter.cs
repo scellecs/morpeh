6c8d0375b1e8df3098838bd22ceb29bd6f1d8875
heymeepo <h3ystudio@gmail.com>
2025-01-11 16:44:43 +0400

    Fix incorrect handling of failed component deserialization

--- Scellecs.Morpeh/Unity/WorldBrowser/Runtime/Serialization/BoxedComponentAdapter.cs
+++ Scellecs.Morpeh/Unity/WorldBrowser/Runtime/Serialization/BoxedComponentAdapter.cs
@@ -1,18 +1,29 @@
 ﻿#if UNITY_EDITOR && MORPEH_REMOTE_BROWSER || DEVELOPMENT_BUILD && MORPEH_REMOTE_BROWSER
 using System;
+using Unity.Collections.LowLevel.Unsafe;
 using Unity.Serialization.Binary;
 using static Scellecs.Morpeh.WorldBrowser.Serialization.SerializationUtility;
 
 namespace Scellecs.Morpeh.WorldBrowser.Serialization {
     internal unsafe sealed class BoxedComponentAdapter : IBinaryAdapter<ComponentDataBoxed> {
-        public void Serialize(in BinarySerializationContext<ComponentDataBoxed> context, ComponentDataBoxed value) {          
+        public void Serialize(in BinarySerializationContext<ComponentDataBoxed> context, ComponentDataBoxed value) {
+            var startLength = context.Writer->Length;
+            context.Writer->Add(0);
+            var contentStart = context.Writer->Length;
+
             WriteString(context, value.data.GetType().AssemblyQualifiedName);
             context.Writer->Add(value.isMarker);
             context.Writer->Add(value.typeId);
             context.SerializeValue(value.data);
+
+            var totalSize = context.Writer->Length - contentStart;
+            UnsafeUtility.MemCpy(context.Writer->Ptr + startLength, &totalSize, sizeof(int));
         }
 
         public ComponentDataBoxed Deserialize(in BinaryDeserializationContext<ComponentDataBoxed> context) {
+            var blockSize = context.Reader->ReadNext<int>();
+            var startOffset = context.Reader->Offset;
+
             var typeName = ReadString(context);
             var isMarker = context.Reader->ReadNext<bool>();
             var typeId = context.Reader->ReadNext<int>();
@@ -22,6 +33,7 @@ namespace Scellecs.Morpeh.WorldBrowser.Serialization {
                 componentData = context.DeserializeValue<IComponent>();
             }
             catch (Exception) {
+                context.Reader->Offset = startOffset + blockSize;
                 var type = Type.GetType(typeName);
                 componentData = (IComponent)Activator.CreateInstance(type);
             }
