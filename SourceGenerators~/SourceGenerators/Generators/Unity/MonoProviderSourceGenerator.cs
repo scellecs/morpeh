f28489a206e7b6b7ae620e3f191f7a431231d14e
Gallardo994 <git@gx994.net>
2025-01-11 13:55:07 +0400

    Incorrect visibility modifier

--- SourceGenerators~/SourceGenerators/Generators/Unity/MonoProviderSourceGenerator.cs
+++ SourceGenerators~/SourceGenerators/Generators/Unity/MonoProviderSourceGenerator.cs
@@ -218,7 +218,7 @@
                 ProviderTypeFullName: monoProviderType.ToDisplayString(SymbolDisplayFormat.FullyQualifiedFormat),
                 ProviderTypeVisibility: monoProviderType.DeclaredAccessibility,
                 StashVariation: MorpehComponentHelpersSemantic.GetStashVariation(monoProviderType),
-                Visibility: monoProviderType.DeclaredAccessibility);
+                Visibility: typeSymbol.DeclaredAccessibility);
         }
 
         private record struct ProviderToGenerate(
