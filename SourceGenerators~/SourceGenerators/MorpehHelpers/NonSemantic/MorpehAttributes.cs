9644e217a64ece9a52ff53c4a314d10e0885d29c
Gallardo994 <git@gx994.net>
2025-01-20 11:40:53 +0400

    Cleanup attributes

--- SourceGenerators~/SourceGenerators/MorpehHelpers/NonSemantic/MorpehAttributes.cs
+++ SourceGenerators~/SourceGenerators/MorpehHelpers/NonSemantic/MorpehAttributes.cs
@@ -34,14 +34,6 @@
         public const string MONO_PROVIDER_NAME      = "MonoProviderAttribute";
         public const string MONO_PROVIDER_FULL_NAME = "Scellecs.Morpeh.Providers.MonoProviderAttribute";
         
-        public const string VALIDATABLE_NAME      = "IValidatable";
-        public const string VALIDATABLE_FULL_NAME = "Scellecs.Morpeh.IValidatable";
-        
-        public const string VALIDATABLE_WITH_GAMEOBJECT_NAME      = "IValidatableWithGameObject";
-        public const string VALIDATABLE_WITH_GAMEOBJECT_FULL_NAME = "Scellecs.Morpeh.IValidatableWithGameObject";
-        
-        public const string LOOP_NAME      = "EcsLoopAttribute"; // Can't have FULL_NAME because it's user-defined.
-        
         public static StringBuilder AppendIl2CppAttributes(this StringBuilder sb, IndentSource indent) {
             sb.Append("#if !").Append(MorpehDefines.MORPEH_ENABLE_IL2CPP_CHECKS).Append(" && ").AppendLine(MorpehDefines.ENABLE_IL2CPP);
             sb.AppendIndent(indent).AppendLine("[global::Unity.IL2CPP.CompilerServices.Il2CppSetOption(global::Unity.IL2CPP.CompilerServices.Option.NullChecks, false)]");
