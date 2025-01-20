b696e632347da9fd1a4da794889c021792db3c41
Gallardo994 <git@gx994.net>
2025-01-20 22:26:41 +0400

    Delete unused

--- SourceGenerators~/SourceGenerators/MorpehHelpers/Semantic/StashSpecialization.cs
+++
@@ -1,7 +0,0 @@
-﻿namespace SourceGenerators.MorpehHelpers.Semantic {
-    public record struct StashSpecialization(
-        string Type, 
-        string GetStashMethod, 
-        string ConstraintInterface
-    );
-}
